6ababe4a015c5548f68c75b308d0285ddb902470
pablolospe <yosoypxl@gmail.com>
2025-05-02 15:36:31 -0300

    Add QueHacemosSection and ContactoSection to main page layout for enhanced content presentation

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,15 @@
+import ContactoSection from "@/components/contacto"
 import NosotrosSection from "@/components/nosotros-section"
 import PuebloBranding from "@/components/pueblo-branding"
+import QueHacemosSection from "@/components/que-hacemos"
 
 export default function Page() {
   return (
     <>
       <PuebloBranding />
       <NosotrosSection />
+      <QueHacemosSection/>
+      <ContactoSection/>
     </>
   )
 }
