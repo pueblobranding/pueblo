54327e56e74923b22528bb90d93b8a2a34cecd78
pablolospe <yosoypxl@gmail.com>
2025-05-13 10:52:32 -0300

    Update QueHacemosSection and EncontramosQueHace components: comment out title in QueHacemosSection for future adjustments and modify margin in EncontramosQueHace for better layout consistency.

--- components/carousel/encontramos-que-hace.tsx
+++ components/carousel/encontramos-que-hace.tsx
@@ -17,7 +17,7 @@ export default function EncontramosQueHace() {
                     </div>
 
                     <div className="text-center text-md md:text-2xl font-bold">
-                    <h1 style={{ fontFamily: 'dream-avenue' }} className="text-white text-3xl md:text-4xl lg:text-6xl text-center mb-16 tracking-wider bg-tigerlily rounded-4xl py-20 mx-40">
+                    <h1 style={{ fontFamily: 'dream-avenue' }} className="text-white text-3xl md:text-4xl lg:text-6xl text-center mb-16 tracking-wider bg-tigerlily rounded-4xl py-20 md:mx-40">
                         ENCONTRAMOS
                         <br />QUE HACE LATIR
                         <br />A TU MARCA
